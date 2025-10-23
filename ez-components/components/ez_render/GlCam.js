4af8fc8752c332457d2e3332543fe5881a1926f3
Sora3e8 <sora3e8@protonmail.com>
2025-10-23 13:41:56 +0200

    Removed debug logs

--- ez-components/components/ez_render/GlCam.js
+++ ez-components/components/ez_render/GlCam.js
@@ -118,8 +118,6 @@ class GlCamManager
 				let z = math.pow(10,k*this.Camera.zoom_speed); 
 				this.Camera.wz = this.Camera.wz * z;
 			}
-			console.log("Zoom write");
-
 		}	
 	}
 
@@ -262,7 +260,6 @@ class GlCam
 			let m_vec = new Mat([0,0,1,0],4).multiply(Mutils.Rotate(this.pitch,this.yaw,this.roll).transpose()).cells;
 			//let lookVec = {x:m_vec[0], y:m_vec[1], z:m_vec[2]};
 			let lookVec = {x:0,y:0,z:1};
-			console.log("Res:",lookVec);
 			let m_proj = Mutils.Parallel_proj(aspect,lookVec,this.wz).cells;
 			gl.uniformMatrix4fv(shader.projectionMatrix, true, new Float32Array(m_proj));
 		}
